04083d31e8b0cb1387a254484d357337fadc982e
Masud Rana <masud.pust16@gmail.com>
2022-06-09 11:48:40 +0600

    Multiple Layout

---
+++ LayoutMVC/Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LayoutMVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        [Route("Employees")]
+        //public ActionResult GetList()
+        //{
+        //    return View("GetList","_Layout");
+        //}
+        public ActionResult GetList()
+        {
+            return View();
+        }
+        [Route("NewEmployee")]
+        //public ActionResult AddEmployee()
+        //{
+        //    return View("AddEmployee", "_Layout");
+        //}
+        public ActionResult AddEmployee()
+        {
+            return View();
+        }
+    }
+}
